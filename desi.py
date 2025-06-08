a1ee4656597f7cb814b9d43cb9bba07f1e18cc45
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-06-08 19:27:07 +0530

    Update desi.py

--- desi.py
+++ desi.py
@@ -30,8 +30,8 @@ def get_random_headers():
     }
 
 # Env vars
-api_id = int(os.environ.get("API_ID", 6701300))
-api_hash = os.environ.get("API_HASH", "006089a340f2806aea683cbfb73kkk")
+api_id = int(os.environ.get("API_ID", 23241238))
+api_hash = os.environ.get("API_HASH", "e6ff6e3068dbea75500865ac49c3608f")
 bot_token = os.environ.get("BOT_TOKEN", "8110926083:AAHuuv8B5V_GIfkyPNdrrs8vZBHL2Gl-i24")
 channel_id = os.environ.get("CHANNEL_ID", "-1002665953559")
 
